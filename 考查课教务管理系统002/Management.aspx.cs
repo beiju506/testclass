d5b3633730c2607459b0de3f115637c10dac6593
beiju506 <506009274@qq.com>
2015-06-22 18:53:16 +0800

    分支2

---
+++ 考查课教务管理系统002/Management.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Management : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void LinkButton_Class_Click(object sender, EventArgs e)
+    {
+        Panel_Class.Visible = true;
+        Panel_User.Visible = false;
+    }
+    protected void LinkButton_User_Click(object sender, EventArgs e)
+    {
+        Panel_Class.Visible = false;
+        Panel_User.Visible = true;
+    }
+}
